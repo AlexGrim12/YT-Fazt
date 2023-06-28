aab7ef3519148e67dfbbbb852528339dcf7c9524
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-06-28 11:32:26 -0600

    fetch API

--- react-principiantes/react-course/src/index.js
+++ react-principiantes/react-course/src/index.js
@@ -5,31 +5,12 @@ import Product, { Navbar } from './Product'
 import { Button } from './Button'
 import { TaskCard } from './task'
 import { Saludar } from './Saludar'
+import { Posts } from './Posts'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const handleChange = (e) => {
-  console.log(e.target.value + '...')
-}
-
 root.render(
   <>
-    <TaskCard ready={true} />
-    <Saludar />
-    <Button text="Saludar" />
-    <input
-      id="hola"
-      onChange={handleChange}
-      onDoubleClick={() => console.log('double click')}
-    />
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-        alert('Enviando datos del form...')
-      }}
-    >
-      <h1>Registro de usuario</h1>
-      <button>Send</button>
-    </form>
+    <Posts />
   </>
 )
