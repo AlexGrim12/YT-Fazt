c049b3b2297e4973f1bc6fa54c512e210dbc745d
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-06-28 12:00:43 -0600

    Arrays

--- react-principiantes/react-course/src/index.js
+++ react-principiantes/react-course/src/index.js
@@ -2,12 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Posts } from './Posts'
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const users = [
+  { id: 1, name: 'Alex', image: 'https://robohash.org/usuario1' },
+  { id: 2, name: 'Leo', image: 'https://robohash.org/usuario2' },
+  { id: 3, name: 'Juan', image: 'https://robohash.org/usuario3' },
+]
 
 root.render(
   <>
-    <Posts />
-    
+    {users.map((user, index) => (
+      <div key={index}>
+        <h1>{user.name}</h1>
+        <img src={user.image} alt="" />
+      </div>
+    ))}
   </>
 )
