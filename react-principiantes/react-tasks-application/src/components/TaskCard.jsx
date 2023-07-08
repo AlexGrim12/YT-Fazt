241d9d68f37dd3c408d9e936379c159bbd2a4e9f
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-07-08 16:36:31 -0600

    context

--- react-principiantes/react-tasks-application/src/components/TaskCard.jsx
+++ react-principiantes/react-tasks-application/src/components/TaskCard.jsx
@@ -1,4 +1,8 @@
-export default function TaskCard({ task, deleteTask }) {
+import { useContext } from 'react'
+import TaskContext from '../context/TaskContext'
+
+export default function TaskCard({ task }) {
+  const { deleteTask } = useContext(TaskContext)
   return (
     <div key={task.id}>
       <h1>{task.title}</h1>
